196c4f1fa001dac31d3975551f7287e639550e5f
sekharpe <psekhar93440@gmail.com>
2023-12-12 15:18:45 +0530

    new file added ok

--- 04-backup.sh
+++ 04-backup.sh
@@ -2,6 +2,6 @@
 
 # backing up the files in a directory 
 pwd
-tar -cvzf *.sh scripts.tar.gz
+tar -cvzf scripts.tar.gz *sh
 var = `ls -lrt | grep gz`
 echo " we have taken the backup of the files to new file called $var"
