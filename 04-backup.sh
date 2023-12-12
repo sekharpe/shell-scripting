48f47fd3408d8e566699c0a6cfb7b2b436ef0309
sekharpe <psekhar93440@gmail.com>
2023-12-12 15:50:40 +0530

    new file added okay2

--- 04-backup.sh
+++ 04-backup.sh
@@ -4,4 +4,7 @@
 pwd
 tar -cvzf scripts.tar.gz *sh
 thefile = $(ls -lrt | grep tar | awk '{print $9}')
-echo "Most recent tar file is: $thefile"
+#echo "Most recent tar file is: $thefile"
+print -r "Most recent screenshot is: $thefile"
+var1 = ls -lrt
+echo $var1
