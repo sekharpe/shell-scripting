a94accf745d587e27322a207e6906c1db992b691
sekharpe <psekhar93440@gmail.com>
2023-12-12 15:53:44 +0530

    new file added okay2

--- 04-backup.sh
+++ 04-backup.sh
@@ -3,8 +3,5 @@
 # backing up the files in a directory 
 pwd
 tar -cvzf scripts.tar.gz *sh
-thefile = $(ls -lrt | grep tar | awk '{print $9}')
-#echo "Most recent tar file is: $thefile"
-print -r "Most recent screenshot is: $thefile"
-var1 = ls -lrt
-echo $var1
+thefile = ls -lrt | grep tar | awk '{print $9}'
+echo "Most recent tar file is: $thefile"
