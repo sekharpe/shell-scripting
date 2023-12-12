e326770e171b2cc5a80fc3a8fc5510a35dabcd21
sekharpe <psekhar93440@gmail.com>
2023-12-12 15:03:01 +0530

    added new file third file new

--- 03-listing_items.sh
+++ 03-listing_items.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # listing the files and folders in a directory
+echo " listing the files in the present working directory"
 ls -lrt
 # list the file names having pattern "sh" in the last
+echo " listing the files having the sh pattern"
 ls -lrt | grep sh
