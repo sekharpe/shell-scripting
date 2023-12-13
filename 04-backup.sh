7004f468861b72dde8a786d49b5982b230a6088c
sekharpe <psekhar93440@gmail.com>
2023-12-13 20:06:26 +0530

    changed structure

--- 04-backup.sh
+++ 04-backup.sh
@@ -3,5 +3,5 @@
 # backing up the files in a directory 
 pwd
 tar -cvzf scripts.tar.gz *sh
-thefile = $(ls -lrt | grep tar | awk '{print $9}')
-echo "Most recent tar file is: $thefile"
+thefile=$(ls -lrt | grep tar | awk '{print $9}')
+echo "the recent tar file is: $thefile"
