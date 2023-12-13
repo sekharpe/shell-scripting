ecd877977813e77a4e5a1cd3c77be558af9bea2b
sekharpe <psekhar93440@gmail.com>
2023-12-13 20:32:11 +0530

    added line

--- 05-count_lines.sh
+++ 05-count_lines.sh
@@ -3,6 +3,9 @@
 #first find the no of lines in a text file
 var=$(ls -lrt)
 for i in var
-thefile=$(ls -lrt | grep sh | awk 'print{$9}')
-count=$(cat $thefile | wc -l)
-echo "the no of lines in the file $thefile is : $count"
+do
+echo "the file is $i"
+done
+#thefile=$((ls -lrt | grep sh | awk 'print{$9}'))
+#count=$(cat $thefile | wc -l)
+#echo "the no of lines in the file $thefile is : $count"
