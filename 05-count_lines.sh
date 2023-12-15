76ee72b6d3260e4a2571bd3990b9de7beb8eea52
sekharpe <psekhar93440@gmail.com>
2023-12-15 12:27:08 +0530

    changed syntax

--- 05-count_lines.sh
+++ 05-count_lines.sh
@@ -17,7 +17,7 @@ read -p "enter the name that you want to search in the file: " search
 #fi
 seach_line=$(grep -q "$search" "$i")
 exit_status=$(echo $?)
-if ($(exit_status) -eq 0); then
+if [ $(exit_status) -eq 0 ]; then
     echo "The word '$search' was found in '$i' it has $count lines in the file and the search pattern line is $search_line ."
 else
     echo "The word '$search' was not found in '$i' it has $count lines in the file and the search pattern was not found."
