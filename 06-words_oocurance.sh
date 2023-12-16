e95ce6735253f722db7002d4dd546a65dab06869
sekharpe <psekhar93440@gmail.com>
2023-12-16 13:12:09 +0530

    add the new count script

---
+++ 06-words_oocurance.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Write a script that reads a text file and counts the occurrences of each word, displaying the top 5 most frequent words along with their counts.
+
+#In a small, small town, there lived a man named John. John was a man of simple, simple tastes and a routine, routine life. Every day, day after day, he would wake up early, early in the morning and go for a walk, walk around the quiet, quiet streets of the town.
+
+
+#var=$(ls -lrt | awk '{print $9}')
+filename=$(ls -lrt | grep wordfile | awk '{print $9}')
+count=$(cat $(filename) | wc -w)
+
+for c in {1..$count}
+do
+match_word=$(awk '{print $c}' $filename)
+search_word=$(cat wordfile | grep -o -i $(match_word) | wc -l)
+echo "the no of occurances of the word $(match_word) is $(search_word)"
+done
+#count=count+1
+
+#awk '{print $1}' $filenam
+
+#cat wordfile | grep -o -i $(match_word) | wc -l #count the no of occurances of a matching word
