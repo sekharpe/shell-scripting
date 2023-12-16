ae8e1c27c89ee7158173c0d1e65361b45d2ed640
sekharpe <psekhar93440@gmail.com>
2023-12-16 13:58:29 +0530

    add the new count script

--- 06-words_oocurance.sh
+++ 06-words_oocurance.sh
@@ -7,9 +7,9 @@
 
 #var=$(ls -lrt | awk '{print $9}')
 #filename=$(ls -lrt | grep wordfile | awk '{print $9}')
-#count=$(cat wordfile | wc -w)
+count=$(cat wordfile | wc -w)
 
-for i in {1..52}
+for i in {1..$count}
 do
 match_word=$(awk -v i="$i" '{print $i}' wordfile)
 #search_word=$(grep -o -i $(match_word) wordfile | wc -l)
